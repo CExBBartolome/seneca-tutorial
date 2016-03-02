8e4b9fdd307e63437a356dca86d1072c9fb4677b
CExBBartolome <bbartolome@i-sight.com>
2016-03-02 11:57:45 -0500

    without plugins

---
+++ index.js
@@ -0,0 +1,21 @@
+var seneca = require('seneca')();
+
+seneca.add({role: 'role', cmd: 'cmd'}, function(args, done) {
+	console.log('1st args:', args);
+	done(null, null);
+});
+
+seneca.add({role: 'role', cmd: 'cmd'}, function(args, done) {
+	console.log('2nd args:', args);
+	args.superExtras = 'superExtras';
+	this.prior(args, function(err, args) {
+		console.log('after prior');
+		done(err, args);
+	});
+});
+
+seneca.act({role: 'role', cmd: 'cmd', extras: 'extras'}, function(err, args) {
+	console.log('original done');
+	console.log('err:', err);
+	console.log('args:', args);
+})
